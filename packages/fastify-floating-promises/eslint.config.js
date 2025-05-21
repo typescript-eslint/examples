fe300ff92d5273a88401bcce80b847997cfe0e8a
Josh Goldberg <git@joshuakgoldberg.com>
2025-05-21 13:12:19 -0400

    Correct eslint.config.js

--- packages/fastify-floating-promises/eslint.config.js
+++ packages/fastify-floating-promises/eslint.config.js
@@ -17,12 +17,15 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-floating-promises": [
         "error", {
-          "allowForKnownSafePromises": [
-            { "from": "package", "name": "FastifyInstance", "package": "fastify" },
-            { "from": "package", "name": "FastifyRegister", "package": "fastify" },
-            { "from": "package", "name": "FastifyReply", "package": "fastify" },
-          ],
-          "checkThenables": true
+          // 1. Switch this to true to start reporting on Fastify Promise types
+          "checkThenables": false,
+
+          // 2. Un-comment this to allow known safe Fastify Promise types
+          // "allowForKnownSafePromises": [
+          //   { "from": "package", "name": "FastifyInstance", "package": "fastify" },
+          //   { "from": "package", "name": "FastifyRegister", "package": "fastify" },
+          //   { "from": "package", "name": "FastifyReply", "package": "fastify" },
+          // ],
         }
       ]
     }
